72530f67463498aa7a4b2411186372eb43b98114
Syed Tousif Ahmed <stahmed@seas.upenn.edu>
2022-03-28 04:09:13 -0400

    Remove theming

--- src/theme/ReactPdf/index.tsx
+++ src/theme/ReactPdf/index.tsx
@@ -16,45 +16,11 @@ export default function ReactPdf(props: ComponentProps<any>): JSX.Element {
   }
 
   return (
-    <>
-      <div >
-        <a href={pdf} className="button button--primary" >Download PDF</a>
-      </div>
-      <div className="card-demo-container">
-        <div className="shadow--md">
-          <Document
-            file={pdf}
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            <Page scale={1.5} pageNumber={pageNumber} />
-          </Document>
-        </div>
-      </div>
-      <nav className="pagination-nav">
-        <div className="pagination-nav__item">
-          <button
-            className="pagination-nav__link"
-            type="button"
-            disabled={pageNumber <= 1}
-            onClick={() => setPageNumber(p => Math.max(1, p - 1))}
-          >
-            <div className="pagination-nav__sublabel">Previous</div>
-            <div className="pagination-nav__label"></div>
-          </button>
-
-        </div>
-        <div className="pagination-nav__item pagination-nav__item--next">
-          <button
-            className="pagination-nav__link"
-            type="button"
-            disabled={pageNumber >= numPages}
-            onClick={() => setPageNumber(p => Math.min(numPages, p + 1))}
-          >
-            <div className="pagination-nav__sublabel">Next</div>
-            <div className="pagination-nav__label">Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}</div>
-          </button>
-        </div>
-      </nav>
-    </>
+    <Document
+      file={pdf}
+      onLoadSuccess={onDocumentLoadSuccess}
+    >
+      <Page scale={1.5} pageNumber={pageNumber} />
+    </Document>
   );
 }
